007ebb005091061446295d09da2f80294fc0b4db
kazit <ktishan3135@bths.edu>
2022-10-11 19:55:56 -0400

    Completed all methods and finished runner code

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
 public class EquationRunner {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        // Step 1 & 2: Asking user for coordinate points
+        System.out.print("Enter the first x-y coordinate point: ");
+        String firstPoint = s.nextLine();
+        System.out.print("Enter the second x-y coordinate point: ");
+        String secondPoint = s.nextLine();
+
+        // Step 3: Parsing out the x and y values of both coordinate points
+        int firstComma = firstPoint.indexOf(",");
+        int x1 = Integer.parseInt(firstPoint.substring(1, firstComma));
+        int y1 = Integer.parseInt(firstPoint.substring(firstComma + 1, firstPoint.length() - 1));
+
+        int secondComma = secondPoint.indexOf(",");
+        int x2 = Integer.parseInt(secondPoint.substring(1, secondComma));
+        int y2 = Integer.parseInt(secondPoint.substring(secondComma + 1, secondPoint.length() - 1));
+
+        // Step 4: Calling the LinearEquation constructor
+        LinearEquation slopeEquation = new LinearEquation(x1, y1, x2, y2);
+
+        // Step 5: Print out the information of the instance
+        slopeEquation.toString();
+
+        // Step 6: Asking the user to input an x-value
+        System.out.print("Enter an x-value: ");
+        double xValue = s.nextDouble();
+
+        //
+        String solvedCoordinatePoint = slopeEquation.solvedCoordinatePoint(xValue);
+        System.out.println("Solved coordinate plane is: " + solvedCoordinatePoint);
+    }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,9 +1,11 @@
 public class LinearEquation {
+    // instance variables:
     private int x1;
     private int y1;
     private int x2;
     private int y2;
 
+    // constructor
     LinearEquation(int x1, int y1, int x2, int y2){
         this.x1 = x1;
         this.y1 = y1;
@@ -11,6 +13,7 @@ public class LinearEquation {
         this.y2 = y2;
     }
 
+    // methods:
     private double slope(){
         double rise = y2 - y1;
         int run = x2 - x1;
@@ -19,8 +22,40 @@ public class LinearEquation {
     }
 
     private int yIntercept(){
-        // y1 = slope() * x1 + b
+        // y = mx + b
+        // y1 = (slope() * x1) + yIntercept
+        int yIntercept = (int) (y1 - (slope() * x1));
+        return yIntercept;
     }
 
+    private String slopeInterceptForm(){
+        String slopeInterceptForm = (slope() + "x + " + yIntercept());
+        return slopeInterceptForm;
+    }
+
+    private double distance(){
+        // a^2 + b^2 = c^2
+        int a = Math.abs(x2 - x1);
+        int b = Math.abs(y2 - y1);
+        double c = Math.sqrt((a * a) + (b * b));
+        return c;
+    }
+
+    // prints all instance information
+    public String toString(){
+        System.out.println("First Pair: (" + x1 + ", " + y1 + ")");
+        System.out.println("Second Pair: (" + x2 + ", " + y2 + ")");
+        System.out.println("Slope of line: " + slope());
+        System.out.println("Y-intercept: " + yIntercept());
+        System.out.println("Slope Intercept Form: " + slopeInterceptForm());
+        System.out.printf("Distance between points: %.2f%n", distance());
+        return null;
+    }
+
+    public String solvedCoordinatePoint(double xValue){
+        double yValue = (slope() * xValue) + yIntercept();
+        String solvedCoordinatePoint = ("(" + xValue + ", " + yValue + ")");
+        return solvedCoordinatePoint;
+    }
 
 }
