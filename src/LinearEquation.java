f74656d0266226870b6fb268f9b82e118fc36c93
BT_4N2_08 <ktishan3135@bths.edu>
2022-10-11 12:31:47 -0400

    Renamed instance variables and added methods

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,15 +1,26 @@
 public class LinearEquation {
-    private int xOne;
-    private int yOne;
-    private int xTwo;
-    private int yTwo;
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
 
     LinearEquation(int x1, int y1, int x2, int y2){
-        xOne = x1;
-        yOne = y1;
-        xTwo = x2;
-        yTwo = y2;
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
     }
 
-    
+    private double slope(){
+        double rise = y2 - y1;
+        int run = x2 - x1;
+        double slope = rise / run;
+        return slope;
+    }
+
+    private int yIntercept(){
+        // y1 = slope() * x1 + b
+    }
+
+
 }
