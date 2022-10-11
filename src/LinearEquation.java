bdc8398d11c822fe8f3cfa538cbf39cdce37e982
unknown <BT_4N2_08@BKTECH.LOCAL>
2022-10-11 12:17:16 -0400

    Initial commit

---
+++ src/LinearEquation.java
@@ -0,0 +1,15 @@
+public class LinearEquation {
+    private int xOne;
+    private int yOne;
+    private int xTwo;
+    private int yTwo;
+
+    LinearEquation(int x1, int y1, int x2, int y2){
+        xOne = x1;
+        yOne = y1;
+        xTwo = x2;
+        yTwo = y2;
+    }
+
+    
+}
